75a2be0a68b0e8284189d550165c295debdf09ee
nelsonfrank <nelsonfr78@outlook.com>
2020-06-10 20:46:27 +0300

    feat: Fetch data from api #5
    Closes #5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  pictures: Object;
 
-  constructor() { }
+  constructor(private _http: HttpService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this._http.getPicture().subscribe((data) => {
+      this.pictures = data;
+    });
   }
-
 }
